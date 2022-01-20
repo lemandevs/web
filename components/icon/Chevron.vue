31e113214ae3fd183b47bb07c8a5b62d6fea804a
Jose Manuel Piñeiro Garcia <josempga@ext.inditex.com>
2022-01-20 21:13:24 +0000

    Add social networks and chevron icons

---
+++ components/icon/Chevron.vue
@@ -0,0 +1,61 @@
+<template>
+  <div :class="classes">
+    <span class="Shape"></span>
+    <span class="Shape"></span>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  active: {
+    type: Boolean,
+    default: false,
+    class: true,
+  },
+  rotate: {
+    type: Boolean,
+    default: false,
+    class: true,
+  },
+})
+const classes = defineClasses('Chevron')
+</script>
+
+<style lang="scss" scoped>
+.Chevron {
+  display: flex;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  &_rotate {
+    transform: rotate(-90deg);
+  }
+  .Shape {
+    position: absolute;
+    width: 66%;
+    height: 20%;
+    border-radius: 9999px;
+    top: 50%;
+    background: currentColor;
+    transition-property: color, transform;
+    transition-timing-function: ease-in-out;
+    transition-duration: 0.3s;
+    &:first-child {
+      left: 0;
+      transform: translateY(-50%) rotate(45deg);
+    }
+    &:last-child {
+      right: 0;
+      transform: translateY(-50%) rotate(-45deg);
+    }
+  }
+  &_active .Shape {
+    &:first-child {
+      transform: translateY(-50%) rotate(-45deg);
+    }
+    &:last-child {
+      transform: translateY(-50%) rotate(45deg);
+    }
+  }
+}
+</style>
---
+++ components/icon/index.vue
@@ -0,0 +1,73 @@
+<template>
+  <i :class="classes">
+    <component :is="icon" :active="active" />
+  </i>
+</template>
+
+<script setup>
+const props = defineProps({
+  name: {
+    type: String,
+    class: true,
+    required: true,
+  },
+  size: {
+    type: String,
+    class: true,
+    default: 'medium',
+    validator(value) {
+      return ['tiny', 'small', 'medium', 'large', 'big', 'huge'].includes(value)
+    },
+  },
+  active: {
+    class: true,
+    type: Boolean,
+  },
+  loading: {
+    class: true,
+    type: Boolean,
+  },
+})
+
+const { name } = toRefs(props)
+
+const icon = computed(() => `Icon${name.value}`)
+
+const classes = defineClasses('Icon')
+</script>
+
+<style lang="scss">
+.Icon {
+  display: inline-flex;
+  &_tiny {
+    width: 0.75rem;
+    height: 0.75rem;
+  }
+  &_small {
+    width: 1rem;
+    height: 1rem;
+  }
+  &_medium {
+    width: 1.25rem;
+    height: 1.25rem;
+  }
+  &_large {
+    width: 1.5rem;
+    height: 1.5rem;
+  }
+  &_big {
+    width: 1.75rem;
+    height: 1.75rem;
+  }
+  &_huge {
+    width: 2rem;
+    height: 2rem;
+  }
+  .Shape,
+  .Shape::before,
+  .Shape::after {
+    color: inherit;
+    background-color: currentColor;
+  }
+}
+</style>
