b699ba4e9e828d5872fc6dd732251bc932bd822f
Jose Manuel Piñeiro Garcia <josempga@ext.inditex.com>
2022-01-27 09:16:59 +0000

    Add design page

---
+++ pages/ui/design.vue
@@ -0,0 +1,24 @@
+<template>
+  <div :class="classes">
+    {{ DesignPage }}
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  prop: {
+    type: String,
+    default: 'value',
+    validator(value) {
+      return true
+    },
+    class: true,
+  },
+})
+const classes = defineClasses('DesignPage')
+</script>
+
+<style lang="scss">
+.DesignPage {
+}
+</style>
