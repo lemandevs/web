db7230ac034829821ecd33f3ef23bf3d3688f749
Jose Manuel Piñeiro Garcia <josempga@ext.inditex.com>
2022-02-02 10:49:50 +0000

    Improve My stack page

--- pages/my-stack.vue
+++ pages/my-stack.vue
@@ -4,23 +4,25 @@
       <div class="Avatar">
         <img class="AvatarPhoto" src="/public/img/profile-photo.png" />
       </div>
-      <div
-        v-for="(tech, index) in stack"
-        class="Tech"
-        :key="tech"
-        :style="`transform: rotate(${(360 / stack.length) * index}deg)`"
-      >
+      <div class="Techs">
         <div
-          class="Point"
-          :style="`transform: rotate(-${(360 / stack.length) * index}deg)`"
+          v-for="(tech, index) in stack"
+          class="Tech"
+          :key="tech"
+          :style="`transform: rotate(${(360 / stack.length) * index}deg)`"
         >
-          <Icon
-            :name="tech"
-            :style="`--item-delay: ${
-              (10000 / stack.length) *
-              ((index + stack.length / 2) % stack.length)
-            }ms`"
-          />
+          <div
+            class="Point"
+            :style="`transform: rotate(-${(360 / stack.length) * index}deg)`"
+          >
+            <Icon
+              :name="tech"
+              :style="`--item-delay: ${
+                (10000 / stack.length) *
+                ((index + stack.length / 2) % stack.length)
+              }ms`"
+            />
+          </div>
         </div>
       </div>
     </div>
@@ -52,39 +54,44 @@ const stack = ref([
 </script>
 
 <style lang="scss">
+@keyframes stop {
+  to {
+    transform: translate3d(0, 0, 0) rotate(0) scale(1);
+  }
+}
 @keyframes moving {
   0% {
-    transform: translate3d(0%, 0%, 0) scale(1);
+    transform: translate3d(0%, 0%, 0) scale(1) rotate(3deg);
   }
   3% {
-    transform: translate3d(-3%, -3%, 0) scale(1.05);
+    transform: translate3d(-3%, -3%, 0) scale(1.05) rotate(4deg);
   }
   6% {
-    transform: translate3d(0%, 0%, 0) scale(1);
+    transform: translate3d(0%, 0%, 0) scale(1) rotate(5deg);
   }
   30% {
-    transform: translate3d(3%, -3%, 0) scale(0.95);
+    transform: translate3d(3%, -3%, 0) scale(0.95) rotate(-1deg);
   }
   40% {
-    transform: translate3d(6%, -6%, 0) scale(1);
+    transform: translate3d(6%, -6%, 0) scale(1) rotate(-4deg);
   }
   50% {
-    transform: translate3d(3%, -3%, 0) scale(0.95);
+    transform: translate3d(3%, -3%, 0) scale(0.95) rotate(-1deg);
   }
   60% {
-    transform: translate3d(0%, -5%, 0) scale(0.98);
+    transform: translate3d(0%, -5%, 0) scale(0.98) rotate(2deg);
   }
   70% {
-    transform: translate3d(3%, -3%, 0) scale(1);
+    transform: translate3d(3%, -3%, 0) scale(1) rotate(4deg);
   }
   80% {
-    transform: translate3d(6%, -1%, 0) scale(1.1);
+    transform: translate3d(6%, -1%, 0) scale(1.05) rotate(1deg);
   }
   90% {
-    transform: translate3d(4%, 0%, 0) scale(1.05);
+    transform: translate3d(4%, 0%, 0) scale(1) rotate(-2deg);
   }
   100% {
-    transform: translate3d(6%, 2%, 0) scale(1);
+    transform: translate3d(6%, 2%, 0) scale(0.98) rotate(1deg);
   }
 }
 .MyStackPage {
@@ -93,28 +100,65 @@ const stack = ref([
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  padding: 4rem 0;
 }
 .Avatar {
-  border: 2px solid var(--color-primary);
   border-radius: 99999px;
-  overflow: hidden;
   width: 300px;
   height: 300px;
+  border: 0.5rem solid transparent;
+  background-clip: padding-box;
+  background-color: var(--color-surface);
+  position: relative;
+  &:before {
+    content: '';
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: -1;
+    margin: -0.5rem;
+    border-radius: inherit;
+    background: linear-gradient(
+      to right,
+      var(--color-primary),
+      var(--color-emphatic)
+    );
+  }
+}
+
+.AvatarPhoto {
+  width: 100%;
+  padding-top: 25%;
+  transform: translateY(-20%);
+  border-radius: 99999px;
+  overflow: hidden;
 }
 .Stack {
   position: relative;
   color: var(--color-primary);
-  padding: 32px;
-  margin-top: 64px;
+  padding: 2rem;
   .Icon {
-    width: 64px;
-    height: 64px;
+    width: 4rem;
+    height: 4rem;
     animation-name: moving;
     animation-duration: 10s;
     animation-iteration-count: infinite;
     animation-timing-function: linear;
     animation-direction: alternate;
     animation-delay: var(--item-delay);
+    transition: all 2s ease;
+    &:hover {
+      animation-name: stop;
+    }
+  }
+  .Techs {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    left: 0;
+    top: 0;
   }
   .Tech {
     position: absolute;
@@ -122,10 +166,12 @@ const stack = ref([
     left: 0;
     width: 100%;
     height: 100%;
+    pointer-events: none;
     .Point {
       position: absolute;
       top: 0;
       left: 0;
+      pointer-events: all;
     }
   }
 }
@@ -141,8 +187,8 @@ const stack = ref([
     }
 
     .Icon {
-      width: 32px;
-      height: 32px;
+      width: 2rem;
+      height: 2rem;
     }
   }
 }
