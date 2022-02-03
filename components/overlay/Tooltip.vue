862795e6014cbcbb23945a478257e99c0e03bde6
Jose Manuel Piñeiro Garcia <josempga@ext.inditex.com>
2022-02-03 20:50:49 +0000

    Improve Tooltip styles

--- components/overlay/Tooltip.vue
+++ components/overlay/Tooltip.vue
@@ -2,15 +2,21 @@
   <OverlayDropdown class="Tooltip">
     <template v-slot:target="{ visible, open, close, toggle }">
       <span
-        @mouseenter="desktop && open()"
-        @mouseleave="desktop && close()"
+        @mouseenter="desktop && debounce(open)"
+        @mouseleave="desktop && debounce(close)"
         @click="toggle"
       >
         <slot name="target" v-bind="{ visible, open, close, toggle }" />
       </span>
     </template>
     <template v-slot:content="{ visible, open, close, toggle }">
-      <CssFlexBox direction="column" align="stretch" gap="1">
+      <CssFlexBox
+        @mouseenter="desktop && debounce(open)"
+        @mouseleave="desktop && debounce(close)"
+        direction="column"
+        align="stretch"
+        gap="1"
+      >
         <slot name="content" v-bind="{ visible, open, close, toggle }" />
         <Btn
           v-if="mobile"
@@ -25,6 +31,11 @@
 </template>
 
 <script setup>
+import _ from 'lodash'
+const debounce = _.debounce((method) => {
+  method()
+}, 300)
+
 const props = defineProps({
   prop: {
     type: String,
@@ -41,15 +52,24 @@ const { mobile, desktop } = provideMediaQueries()
 
 <style lang="scss">
 .Tooltip {
-  padding: 1rem 0;
   .DropdownContent {
     border-radius: 0.5rem !important;
     overflow: hidden;
     padding: 1rem;
+    margin-top: 1rem;
   }
   &.Dropdown_middle::after {
     left: 50%;
     transform: translate3d(-50%, -1rem, 0);
+  }
+  @media screen and (max-width: 769px) {
+    .DropdownContent {
+      padding: 1.5rem 1rem;
+      i {
+        display: block;
+        padding: 1.5rem 1rem;
+      }
+    }
   }
   @media screen and (min-width: 769px) {
     &::after {
