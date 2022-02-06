42ec70849d3964a67cfac3074386b6654feb2500
Jose Manuel Piñeiro Garcia <josempga@ext.inditex.com>
2022-02-06 21:43:21 +0000

    Improve stack async fetch

--- pages/my-stack.vue
+++ pages/my-stack.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="classes">
-    <Roulette :items="stack" />
+    <Roulette v-if="stack" :items="stack" />
   </div>
 </template>
 
@@ -14,76 +14,14 @@ definePageMeta({
   transition: { appear: true, name: 'Appear', duration: 200, mode: 'out-in' },
 })
 const classes = defineClasses('MyStackPage')
-
-const stack = ref([
-  {
-    id: 'node',
-    name: 'NodeJS',
-    description: "I'm really familiar with NodeJS and all its ecosystem.",
-    url: 'https://nodejs.org/',
-  },
-  {
-    id: 'js',
-    name: 'JavaScript',
-    description:
-      'I am confident of my Javascript skills and am always learning more',
-    url: 'https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/What_is_JavaScript',
-  },
-  {
-    id: 'html',
-    name: 'HTML',
-    description:
-      'Obviously I have knowledge on the standard markup language for Web pages, what else?',
-    url: 'https://www.w3schools.com/html/',
-  },
-  {
-    id: 'css',
-    name: 'CSS',
-    description:
-      "<p>I like like CSS, i have SASS knowlege and usually use frameworks like tailwind, bulma, bootstrap (etc, etc...) but if I'm honest I prefer to code, if possible of course </p><p><i>CSS is Awesome, it can be as lovely as poetry or frightening like a scary movie.</i></p>",
-    url: 'https://css-tricks.com/',
-  },
-  {
-    id: 'vue',
-    name: 'Vue',
-    description:
-      'I confess, I am a Vue lover. I learned to program web applications with Vue in 2016 (Vue 1.x) and since then I have not stopped doing it.',
-    url: 'https://vuejs.org/',
-  },
-  {
-    id: 'nuxt',
-    name: 'NuxtJS',
-    description: 'This Web is been builded using NuxtJS.',
-    url: 'https://nuxtjs.org/',
-  },
-  {
-    id: 'react',
-    name: 'ReactJS',
-    description:
-      'The last 4 years I have been programming web applications using React.',
-    url: 'https://reactjs.org/',
-  },
-  {
-    id: 'nextjs',
-    name: 'NextJS',
-    description: 'Best framework to build React application easy and fast!',
-    url: 'https://redux.js.org/',
-  },
-  {
-    id: 'redux',
-    name: 'Redux',
-    description:
-      'I think there are currently alternatives to handle application states',
-    url: 'https://redux.js.org/',
-  },
-  {
-    id: 'graphql',
-    name: 'GraphQL',
-    description:
-      "I use GraphQL and I think is a good solution to implement API's but REST is still The King of API's",
-    url: 'https://reactjs.org/',
-  },
-])
+const stack = ref(null)
+onMounted(() => {
+  fetch('/api/stack')
+    .then((response) => response.json())
+    .then((response) => {
+      stack.value = response
+    })
+})
 </script>
 
 <style lang="scss">
